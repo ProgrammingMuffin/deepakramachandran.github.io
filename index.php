70db063dc5e7af609658ed7d6cfbebb9501819dc
Deepak Ramachandran <42649107+ProgrammingMuffin@users.noreply.github.com>
2018-12-25 14:10:00 +0530

    Portfolio development
    portfolio development

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <link href="stylesheets/index.css" type="text/css" rel="stylesheet" />
+    <link href="bootstrap/css/bootstrap.min.css" type="text/css" rel="stylesheet" />
+    <link href="bootstrap/js/bootstrap.min.js" type="text/javascript" />
+</head>
+<body>
+    <div class="container_topbar" >
+        <div class="topbar" >
+            <div class="topbar_logo" >Deepak Ramachandran</div>
+            <div class="topbar_button" >Contact Me</div>
+            <div class="topbar_button" >Projects</div>
+            <div class="topbar_button" >Home</div>
+        </div>
+    </div>
+</body>
+</html>
